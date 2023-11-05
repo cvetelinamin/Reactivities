73c72cd115ed9d16478510aadbef2bb9b24568b5
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2023-11-05 20:39:31 +0200

    Add detail view and form for activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import { Grid, List } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
     activities : Activity[];
+    selectedActivity : Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSeletActivity: () => void;
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({activities, selectedActivity, selectActivity, cancelSeletActivity}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities}/>
+                <ActivityList activities={activities} selectActivity={selectActivity} />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                {selectedActivity && 
+                <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSeletActivity}/>}
+                <ActivityForm/>
             </Grid.Column>
         </Grid>
     )
