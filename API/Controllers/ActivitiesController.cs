5cb84ccaace499342823939b22d676c502775236
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2023-11-21 23:08:18 +0200

    FluentValidation and handling errors

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -12,22 +12,21 @@ namespace API.Controllers
     {
 
         [HttpGet] //api/activities
-        public async Task<ActionResult<List<Activity>>> GetActivities() 
+        public async Task<IActionResult> GetActivities() 
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        public async Task<IActionResult> GetActivity(Guid id)
         {
-            return await Mediator.Send(new Details.Query{Id =id});
+            return HandleResult(await Mediator.Send(new Details.Query{Id =id}));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            await Mediator.Send(new Create.Command { Activity = activity });
-            return Ok();
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")]
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -6,23 +6,24 @@ using MediatR;
 using Domain;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using Application.Core;
 
 namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>> {}
+        public class Query : IRequest<Result<List<Activity>>> {}
 
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext context;
             public Handler(DataContext context)
             {
                 this.context = context;
             }
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await this.context.Activities.ToListAsync();
+                return Result<List<Activity>>.Success(await this.context.Activities.ToListAsync());
             }
         }
     }
