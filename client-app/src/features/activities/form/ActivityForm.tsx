954d8173748da696a314d7e55fc89582cbb57b09
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2023-12-10 22:54:04 +0200

    Create reusable date and select input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,10 +5,13 @@ import { observer } from 'mobx-react-lite'
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
-import { Formik, Form, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from 'yup';
-import MyTextInput from "../../../app/common/form/MytextInput";
+import MyTextInput from "../../../app/common/form/MyTextInput";
 import MyTextArea from "../../../app/common/form/MyTextArea";
+import MySelectInput from "./MySelectInput";
+import { categoryOptions } from "../../../app/common/options/categoryOptions";
+import MyDateInput from "../../../app/common/form/MyDateInput";
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
@@ -64,8 +67,8 @@ export default observer(function ActivityForm() {
                     <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name="title" placeholder="Title"/>
                         <MyTextArea rows={3} placeholder='Description'name="description" />
-                        <MyTextInput placeholder='Category'name=" category" />
-                        <MyTextInput placeholder='Date' name="date" />
+                        <MySelectInput options={categoryOptions} placeholder='Category'name=" category" />
+                        <MyDateInput placeholderText='Date' name="date" showTimeSelect timeCaption="time" dateFormat='MMMM d. yyyy h:mm aa'/>
                         <MyTextInput placeholder='City' name="city" />
                         <MyTextInput placeholder='Venue' name="venue" />
                         <Button loading={loading} floated="right" positive type="submit" content="Submit"/>
