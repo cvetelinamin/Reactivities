a4391150458dc5c061403197ee8c0fcf1c40bd14
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2024-01-07 21:46:28 +0200

    Adding custom auth policy

--- API/Extensions/IdentityServicesExtensions.cs
+++ API/Extensions/IdentityServicesExtensions.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
@@ -34,7 +36,12 @@ namespace API.Extensions
                             ValidateAudience = false
                         };
                     });
-
+            services.AddAuthorization(opt => {
+                opt.AddPolicy("IsActivityHost", policy => {
+                    policy.Requirements.Add(new IsHostRequirement());
+                });
+            });
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
             services.AddScoped<TokenServices>();
 
             return services; 
