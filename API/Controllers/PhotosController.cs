0c0e905d01a47dfe83e4484b2c356095de7f5aef
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2024-01-23 22:50:45 +0200

    Setting main photo and returning user profiles

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Photos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace API.Controllers
 {
@@ -19,6 +20,12 @@ namespace API.Controllers
         public async Task<IActionResult> Delete(string id)
         {
             return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain(string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMain.Command{Id = id}));
         }
     }
 }
