2fd265f25a6bad2db0d4660d9406610047865d6c
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2024-02-27 22:26:50 +0200

    Client side filtering

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -12,7 +12,7 @@ namespace Application.Activities
     {
         public class Query : IRequest<Result<PagedList<ActivityDto>>> 
         {
-            public PagingParams Params { get; set; }
+            public ActivityParams Params { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
@@ -29,9 +29,20 @@ namespace Application.Activities
             public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var query = this.context.Activities
+                                .Where(d => d.Date >= request.Params.StartDate)
                                 .OrderBy(d => d.Date)
                                 .ProjectTo<ActivityDto>(this.mapper.ConfigurationProvider, new {currentUsername = this.userAccessor.GetUsername()})
                                 .AsQueryable();
+
+                if(request.Params.IsGoing && !request.Params.IsHost) 
+                {
+                    query = query.Where(x => x.Attendees.Any(a => a.Username == this.userAccessor.GetUsername()));
+                }
+
+                if(request.Params.IsHost && !request.Params.IsGoing)
+                {
+                    query = query.Where(x => x.HostName == this.userAccessor.GetUsername());
+                }
 
    //             var activitiesToReturn = this.mapper.Map<PagedList<ActivityDto>>(query);
                 return Result<PagedList<ActivityDto>>.Success(
